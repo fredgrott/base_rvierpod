c750aab8a07cbb2b1e181fd90aac5bbb64fdf022
Fred Grott <fred.grott@gmail.com>
2021-04-07 07:05:48 -0500

    some more component themes
    some more componenet themes

--- lib/app/themes/my_materialappbardata.dart
+++ lib/app/themes/my_materialappbardata.dart
@@ -2,13 +2,19 @@
 // Use of this source code is governed by a BSD-style license.
 
 
-import 'package:base_riverpod/app/themes/my_materialtext_theme.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 MaterialAppBarData myMaterialAppBarData = MaterialAppBarData(
-  textTheme: myMaterialTextTheme,
+  textTheme: myMaterialTitleTextTheme ,
   // to adjust to iOS which defaults to title centered when no icons are present
   centerTitle: true,
   brightness: Brightness.light,
+  
+);
+
+TextTheme myMaterialTitleTextTheme = const TextTheme(
+  headline6: TextStyle(color: Colors.black,
+                       fontWeight: FontWeight.bold  )
 );
